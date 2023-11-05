b8e7355783f165e466837051ed6813c82e11ee2e
i564407 <hab.yang@sap.com>
2023-11-06 01:48:06 +0800

    create blog project

---
+++ blog/src/test/java/com/hab/blog/HelloControllerTests.java
@@ -0,0 +1,25 @@
+package com.hab.blog;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HelloControllerTests {
+
+    @Autowired
+    private  MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/hello")).
+                andExpect(MockMvcResultMatchers.status().isOk()).
+                andExpect(MockMvcResultMatchers.content().string("Hello, World!"));
+    }
+}
