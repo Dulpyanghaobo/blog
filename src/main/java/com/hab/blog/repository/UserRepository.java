6838690e55c0662d9ff48f886a06745bbd1385be
i564407 <hab.yang@sap.com>
2023-11-06 23:18:07 +0800

    add test

--- src/main/java/com/hab/blog/repository/UserRepository.java
+++ src/main/java/com/hab/blog/repository/UserRepository.java
@@ -4,7 +4,11 @@ import com.hab.blog.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
     // 这里可以定义查询方法，Spring Data JPA会自动实现
 }
--- src/test/java/com/hab/blog/BlogApplicationTests.java
+++ src/test/java/com/hab/blog/BlogApplicationTests.java
@@ -1,6 +1,7 @@
 package com.hab.blog;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
