e8b9f06fd384c0d675a96013badb00635d606e51
Dulpyanghaobo <2533382191@qq.com>
2024-09-30 22:50:48 +0800

    update

--- src/main/java/com/hab/blog/feature/v1/User/UserController.java
+++ src/main/java/com/hab/blog/feature/v1/User/UserController.java
@@ -3,7 +3,6 @@ package com.hab.blog.feature.v1.User;
 import com.hab.blog.feature.v1.User.Dto.UserProfileDto;
 import com.hab.blog.feature.v1.User.Dto.UserProfileUpdateDto;
 import com.hab.blog.feature.v1.User.Service.UserService;
-import com.hab.blog.feature.v1.response.ApiResponse;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -38,10 +37,22 @@ public class UserController {
         // 从 SecurityContext 中获取当前用户
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String username = authentication.getName();
-
         userService.updateUserProfile(username, profileUpdateDto);
-
         return ResponseEntity.ok("Profile updated successfully");
     }
+//    绑定手机号	POST	/api/v1/user/bind/phone	绑定用户手机号
+    // 检查是否绑定了手机号
+    // 如果绑定了 先调用api解绑手机号
+    // 否则直接绑定手机号
+    // 绑定手机号
+        // 请求 api 获取验证码
+        // 用户输入验证码校验， 校验成功
+            // 绑定成功
+            // 否则绑定失败
+
+//    解绑手机号	POST	/api/v1/user/unbind/phone	解绑用户手机号
+        // 检查用户是否绑定了手机号，如果绑定了则调用发送验证码api解绑
+        // 用户输入验证码， 调用验证解绑api然后解除绑定， user表里面的手机号为null
 }
 
+// springboot + gradle + JDK20
